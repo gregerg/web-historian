3c99e6a8cf31d65aec8ea3ae6cac531617fb7588
gregerg <gregerg@gmail.com>
2014-10-01 16:18:00 -0700

    handles saving to the sites.txt and hashtable

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,15 +1,18 @@
 var http = require("http");
 var handler = require("./request-handler");
 var express = require("express");
+var bodyParser = require('body-parser')
 // TODO: requirements for request handler
 var archive = require('../helpers/archive-helpers');
-
 
 
 var app = express();
 var server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port);
 });
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use( bodyParser.urlencoded() ); // to support URL-encoded bodies
 
 
 app.use(express.static(__dirname + '/public'));
--- web/request-handler.js
+++ web/request-handler.js
@@ -8,33 +8,56 @@ var router = express.Router();
 var sitesPath = __dirname+'/../archives/sites.txt';
 var dataPath = __dirname+'/../archives/sites';
 
+var index = {
+
+};
+var makeIndex = function(){
+  fs.readFile(sitesPath,{encoding:'utf8'},function(err,data){
+    if(err) throw err;
+    var lines = data.split('\n');
+    var found = false;
+    for(var i = 0; i < lines.length; i++){
+      index[lines[i]] = i;
+    }
+    console.log('Successfully populated index!');
+  });
+};
+
+makeIndex();
+
 router.get('/',function(req,res){
   // res.send(200,archive.paths.list);
   res.status(200).send('<input>');
 });
-
 router.get('/:domain',function(req,res){
   var domain = req.params.domain;
-  fs.readFile(sitesPath,{encoding:'utf8'},function(err,data){
-    if(err) res.send(500,err+'Our sites index exploded!');
-    var lines = data.split('\n');
-    var found = false;
-    for(var i = 0; i < lines.length; i++){
-      if(lines[i]===domain){
-        found = true;
-        res.status(200).send('hurray');
-      }
-    }
+  if(index.hasOwnProperty(domain)){
+    var filePath = dataPath+'/'+domain;
 
-    if(!found){
-      res.status(404).send('Oh no!');
-    }
+    fs.readFile(filePath,{encoding:'utf8'},function(err,data){
+      if(err) throw err;
+      res.status(200).send(data);
+    });
 
-  });
+  }else{
+    res.status(404).send('It is not there');
+  }
 });
-router.post('/:domain',function(req,res){
-  var domain = req.params.domain;
-
+router.post('/',function(req,res){
+  var domain = req.body.url;
+  if(index.hasOwnProperty(domain)){
+    // redirect to our get.
+      res.redirect(302,'/'+domain);
+  }else{
+    // create it! (add it to the archive)
+    var temp = '\n'+domain;
+    fs.appendFile(sitesPath,temp,function(err){
+      if(err) res.status(500).send('Our index exploded far!')
+      console.log('Someone saved a file on us!');
+      index[domain] = true;
+      res.redirect(302,'/loading.html');
+    })
+  }
 });
 
 
