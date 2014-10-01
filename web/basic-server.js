3df00ee0b90814783302c8d6cd883a2706261e9f
gregerg <gregerg@gmail.com>
2014-10-01 14:42:47 -0700

    upstream pull, and added some express stuff

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,18 @@
 var http = require("http");
 var handler = require("./request-handler");
+var express = require("express");
+// TODO: requirements for request handler
+var archive = require('../helpers/archive-helpers');
 
-var port = 8080;
-var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
 
+
+var app = express();
+var server = app.listen(8080, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
+app.get('/',function(req,res){
+
+  // res.send(200,archive.paths.list);
+  res.status(200).send('<input>');
+});
